47d39c2315d40763cdcfdb86b09cfb0628d0b7a6
bandy <my@example.com>
2021-06-29 20:26:07 +0300

    alksjdhlkajsdh

---
+++ myzip.py
@@ -0,0 +1,11 @@
+def my_zip(x1, x2):
+	length = min(len(x1), len(x2))
+	for i in range(length):
+		yield(x1[i], x2[i])
+
+x1 = [1, 2, 3, 4, 5]
+names = ('John', 'James', "Jack")
+
+for res in my_zip(names, x1):
+	print(res)
+
