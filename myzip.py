ec3da95df7c69fd64f5df3d1fdf6ad229cfe7fb8
OldKorshunov <dmkorshunov2013@gmail.com>
2021-08-06 16:56:35 +0300

    another example of zip function

--- myzip.py
+++ myzip.py
@@ -9,3 +9,20 @@ names = ('John', 'James', "Jack")
 for res in my_zip(names, x1):
 	print(res)
 
+
+//another example
+
+def short_seq_range(*args):
+    return range(min(map(len, args)))
+
+
+def Task4():
+    a = 'asdfgh'
+    b = (10, 20, 30, 40)
+    c = ((a[i], b[i])
+    for i in short_seq_range(a, b) )
+
+    for item in c:
+        print(item)
+
+Task4()
